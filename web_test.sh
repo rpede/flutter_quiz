ce369b8ff3c359e8ac9fc0d5df248606a43d2953
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-02-21 18:27:14 +0100

    Download chromedriver

--- web_test.sh
+++ web_test.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # See https://docs.flutter.dev/cookbook/testing/integration/introduction#5b-web
+CHROME_VERSION="122.0.6261.57"
 
 # If "debug" arg is given we run in brower, otherwise run headless
 if [ "$1" = "debug" ]; then
@@ -8,18 +9,24 @@ if [ "$1" = "debug" ]; then
 else
   DRIVER=web-server
 fi
+echo "Driver $DRIVER"
 
-# Download chromedriver
-pushd test_driver
-CHROMEDRIVER=chromedriver-linux64.zip
-if [ ! -f "$CHROMEDRIVER" ]; then
-    wget https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/linux64/chromedriver-linux64.zip
-    unzip -n chromedriver-linux64.zip chromedriver-linux64/chromedriver
+# Download chromedriver to ./test_driver
+# linux64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/linux64/chromedriver-linux64.zip
+# mac-arm64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/mac-arm64/chromedriver-mac-arm64.zip
+# mac-x64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/mac-x64/chromedriver-mac-x64.zip
+# win32	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/win32/chromedriver-win32.zip
+# win64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/win64/chromedriver-win64.zip
+if ! (type chromedriver &> /dev/null) || [ ! -f "chromedriver" ]; then
+  dart run test_driver/download_chromedriver.dart 
 fi
 
 # Start chromedriver
-chromedriver --port=4444 &
-popd
+if (type chromedriver &> /dev/null); then
+  ./chromedriver --port=4444 &
+else
+  chromedriver --port=4444 &
+fi
 
 # Run tests
 flutter drive \
