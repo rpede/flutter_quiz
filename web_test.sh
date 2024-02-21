a351984ee4ed8edfb3c766ea7beb1bd9cb849e7f
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2024-02-21 20:16:56 +0100

    Cleanup

--- web_test.sh
+++ web_test.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 # See https://docs.flutter.dev/cookbook/testing/integration/introduction#5b-web
-CHROME_VERSION="122.0.6261.57"
 
 # If "debug" arg is given we run in brower, otherwise run headless
 if [ "$1" = "debug" ]; then
@@ -12,12 +11,7 @@ fi
 echo "Driver $DRIVER"
 
 # Download chromedriver to ./test_driver
-# linux64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/linux64/chromedriver-linux64.zip
-# mac-arm64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/mac-arm64/chromedriver-mac-arm64.zip
-# mac-x64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/mac-x64/chromedriver-mac-x64.zip
-# win32	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/win32/chromedriver-win32.zip
-# win64	https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.57/win64/chromedriver-win64.zip
-if ! (type chromedriver &> /dev/null) || [ ! -f "chromedriver" ]; then
+if ! (type chromedriver &> /dev/null) && [ ! -f "chromedriver" ]; then
   dart run test_driver/download_chromedriver.dart 
 fi
 
